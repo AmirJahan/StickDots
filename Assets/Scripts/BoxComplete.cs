12a34eac06be6339d71088651fd4a5217feec75e
Yewon Jung <pg25yewon@vfs.com>
2024-05-23 09:18:09 -0700

    there is issue of box capture still

--- Assets/Scripts/BoxComplete.cs
+++ Assets/Scripts/BoxComplete.cs
@@ -20,6 +20,9 @@ public class BoxComplete : MonoBehaviour
 
     private int col;
     private int winPlayer;
+
+    private Queue<Vector3> boxesToAnimate = new Queue<Vector3>();
+
     //[SerializeField] Player winPlayer;
     //[SerializeField] int col;
     //[SerializeField] int x, y;
@@ -46,18 +49,21 @@ public class BoxComplete : MonoBehaviour
         return x + (y * (col));
     }
 
-    public void PlayCaptureBoxAnim(Vector3 boxCoordAndCapturedBy) 
+    public void PlayCaptureBoxAnim(Vector3 boxCoordAndCapturedBy)
     {
         int x = (int)boxCoordAndCapturedBy.x;
-        int y = (int)boxCoordAndCapturedBy .y;
+        int y = (int)boxCoordAndCapturedBy.y;
         winPlayer = (int)boxCoordAndCapturedBy.z;
+
         //get the specific gameobject render by hash code
         Renderer ren = boxBackgrounds[getBoxHash(x, y)].gameObject.GetComponent<Renderer>();
 
+        // Activate the game object
         ren.gameObject.SetActive(true);
+
+        // Set the active color for animation
         activeColor = new Material(completionShader);
-        activeColor.SetColor("_baclgroundColor",
-            GamePlayManager.Instance.players[winPlayer].myColor);
+        activeColor.SetColor("_backgroundColor", GamePlayManager.Instance.players[winPlayer].myColor);
         ren.material = activeColor;
         blingMode = true;
 
@@ -65,7 +71,48 @@ public class BoxComplete : MonoBehaviour
         {
             AudioSource.PlayClipAtPoint(audioClip, transform.position);
         }
+
+        // Add the box coordinate to the queue
+        boxesToAnimate.Enqueue(boxCoordAndCapturedBy);
+
+        // If this is the first box to animate, start the animation coroutine
+        if (boxesToAnimate.Count == 1)
+        {
+            float delayBetweenAnimations = 2.5f;
+            StartCoroutine(AnimateNextBoxAfterDelay(delayBetweenAnimations));
+        }
     }
+
+    private IEnumerator AnimateNextBoxAfterDelay(float delay)
+    {
+        while (boxesToAnimate.Count > 0)
+        {
+            // Wait for the specified delay
+            yield return new WaitForSeconds(delay);
+
+            // Get the next box to animate
+            Vector3 nextBoxCoord = boxesToAnimate.Dequeue();
+
+            // Get the specific gameobject renderer by hash code
+            Renderer ren = boxBackgrounds[getBoxHash((int)nextBoxCoord.x, (int)nextBoxCoord.y)].gameObject.GetComponent<Renderer>();
+
+            // Activate the game object
+            ren.gameObject.SetActive(true);
+
+            // Set the active color for animation
+            activeColor = new Material(completionShader);
+            activeColor.SetColor("_backgroundColor", GamePlayManager.Instance.players[winPlayer].myColor);
+            ren.material = activeColor;
+            blingMode = true;
+
+            // If audio clip is provided, play it
+            if (audioClip != null)
+            {
+                AudioSource.PlayClipAtPoint(audioClip, transform.position);
+            }
+        }
+    }
+
 
     private void ResetBling()
     {
@@ -77,10 +124,8 @@ public class BoxComplete : MonoBehaviour
         if (blingMode)
         {
             float offset = activeColor.GetFloat("_HighLightOffset");
-            activeColor.SetFloat("_HighLightOffset", offset + Time.deltaTime/3.0f);
+            activeColor.SetFloat("_HighLightOffset", offset + Time.deltaTime / 3.0f);
             if (offset >= 1.0f) ResetBling();
         }
-
-        
     }
 }
